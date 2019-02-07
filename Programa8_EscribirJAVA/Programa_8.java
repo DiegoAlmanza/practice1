38ec318e6e55433dd7bbab3ca6aefac57605c729
DiegoAlmanza <46726320+DiegoAlmanza@users.noreply.github.com>
2019-02-07 13:44:37 -0700

    Add files via upload

---
+++ Programa8_EscribirJAVA/Programa_8.java
@@ -0,0 +1,16 @@
+//Nombre: Diego Almanza Beyless
+//Dia: 07/02/2019
+//Hora: 1:24
+
+
+class programa8 {
+    public static void main(String[] args) {
+//Se imprime JAVA por partes. 
+	System.out.print("   J    a    V     V  a     \n");
+	System.out.print("   J   a a    V   V  a a   \n");
+	System.out.print("J  J  aaaaa    V V  aaaaa  \n");
+	System.out.print(" JJ  a     a    V  a     a   \n");
+
+
+}
+}
