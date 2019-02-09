50ebd8d16e2b303a2265804ef65cbbb39d942dda
DiegoAlmanza <46726320+DiegoAlmanza@users.noreply.github.com>
2019-02-08 17:21:15 -0700

    Add files via upload

---
+++ Programa10_Formula/programa10_Formula.java
@@ -0,0 +1,12 @@
+//Nombre: Diego Almanza Beyless
+//Dia: 08/02/2019
+//Hora: 17:14
+ 
+class programa10 {
+    public static void main(String[] args) {
+	double r;
+	r=4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
+	System.out.print("4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11))\n");
+	System.out.print("\n Resultado: "+r); 		
+}
+}
