0a0d6be10a8f2578f1df15308921514b00bb93af
DiegoAlmanza <46726320+DiegoAlmanza@users.noreply.github.com>
2019-02-08 17:11:59 -0700

    Add files via upload

---
+++ Programa 9/programa9_expresion.java
@@ -0,0 +1,11 @@
+//Nombre: Diego Almanza Beyless
+//Dia: 08/02/2019
+//Hora: 16:46
+ 
+class programa9 {
+    public static void main(String[] args) {
+	
+	System.out.print("((25.5*3.5-3.5*3.5)/(40.5-4.5))\n");
+	System.out.print("Resultado: "+((25.5*3.5-3.5*3.5)/(40.5-4.5)) );		
+}
+}
