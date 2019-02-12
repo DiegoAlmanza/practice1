e56de90914d48c0277a99d77e0684c69ebf2851a
DiegoAlmanza <46726320+DiegoAlmanza@users.noreply.github.com>
2019-02-12 12:29:17 -0700

    Add files via upload

---
+++ Programa 16_printface/programa16_printface.java
@@ -0,0 +1,15 @@
+////Nombre: Diego Almanza Beyless
+//Dia: 12/02/2019
+//Hora: 12:17
+
+class programa16{
+public static void main(String[] args){
+//Se impreme por partes el dibijo
+	System.out.print("     +\"\"\"\"\"+  \n"); //Se separa con diagonal las comillas para poder imprimirlas en pamtalla.                                                
+	System.out.print("    [| o o |]      \n");                                               
+ 	System.out.print("     |  ^  |       \n");                                                
+ 	System.out.print("     | '-' |       \n");                                                
+ 	System.out.print("     +-----+       \n");
+		
+} 
+}
