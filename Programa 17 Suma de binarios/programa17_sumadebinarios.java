23e2e56fe98cb58e04f7e889765000c17521123c
DiegoAlmanza <46726320+DiegoAlmanza@users.noreply.github.com>
2019-02-16 15:12:26 -0600

    Update programa17_sumadebinarios.java

--- Programa 17 Suma de binarios/programa17_sumadebinarios.java
+++ Programa 17 Suma de binarios/programa17_sumadebinarios.java
@@ -4,7 +4,7 @@
  import java.util.Scanner;//Importamos la libreria java.util para usar la clase Scanner para poder leer valores de entrada 
 
 class programa17 {//Se declara la clase 
-    public static void main(String[] args) {//
+    public static void main(String[] args) {
 	
 	Scanner sc= new Scanner(System.in);//Se crea el Scanner para poder leer valores desde el teclado.
 	System.out.print("Ingrese el primer numero binario: ");//Pedimos que se ingrese el primer valor. 
@@ -13,7 +13,7 @@ class programa17 {//Se declara la clase
 	System.out.print("Ingrese el segundo numero binario: ");//Pedimos que se ingrese el segundo valor. 
 	String b=sc.nextLine();////El Scanner permite ingresar el numero al usuario y almacenarlo en b.	
 
-	int a1 = Integer.parseInt(a, 2);//La funcion Integer.parseInt convierte la cadena a un numero entero 
+	int a1 = Integer.parseInt(a, 2);//La funcion Integer.parseInt convierte la cadena a un numero entero y la base 2 lo hace binario.
         int b1 = Integer.parseInt(b, 2);
         int c1 = a1 + b1;//Sumamos los dos numeros 
 
