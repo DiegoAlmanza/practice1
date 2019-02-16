8c3ea267ba2cda35a848692cd2a6b67e35c47817
DiegoAlmanza <46726320+DiegoAlmanza@users.noreply.github.com>
2019-02-15 21:07:31 -0600

    Update programa17_sumadebinarios.java

--- Programa 17 Suma de binarios/programa17_sumadebinarios.java
+++ Programa 17 Suma de binarios/programa17_sumadebinarios.java
@@ -1,23 +1,23 @@
 //Nombre: Diego Almanza Beyless
 //Dia: 14/02/2019
 //Hora: 12:14
- import java.util.Scanner;//Nos permite crear el Scaner para poder leer valores de entrada de varios tipos.
+ import java.util.Scanner;//Importamos la libreria java.util para usar la clase Scanner para poder leer valores de entrada 
 
-class programa17 {
-    public static void main(String[] args) {
+class programa17 {//Se declara la clase 
+    public static void main(String[] args) {//
 	
-	Scanner sc= new Scanner(System.in);//Se crea el Scanner para poder ingresar datos desde el teclado y poder almacenarlos.
-	System.out.print("Ingrese el primer numero binario: ");
-	String a=sc.nextLine();
+	Scanner sc= new Scanner(System.in);//Se crea el Scanner para poder leer valores desde el teclado.
+	System.out.print("Ingrese el primer numero binario: ");//Pedimos que se ingrese el primer valor. 
+	String a=sc.nextLine();//El Scanner permite ingresar el numero al usuario y almacenarlo en a.
 
-	System.out.print("Ingrese el segundo numero binario: ");
-	String b=sc.nextLine();	
+	System.out.print("Ingrese el segundo numero binario: ");//Pedimos que se ingrese el segundo valor. 
+	String b=sc.nextLine();////El Scanner permite ingresar el numero al usuario y almacenarlo en b.	
 
-	int a1 = Integer.parseInt(a, 2);//
+	int a1 = Integer.parseInt(a, 2);//La funcion Integer.parseInt convierte la cadena a un numero entero 
         int b1 = Integer.parseInt(b, 2);
-        int c1 = a1 + b1;
+        int c1 = a1 + b1;//Sumamos los dos numeros 
 
-        String r = Integer.toBinaryString(c1);
-	System.out.print("Resultado: "+r);
+        String r = Integer.toBinaryString(c1);//Esta funcion convierte la suma a binario. 
+	System.out.print("Resultado: "+r);//Mostramos el resultado 
 }
 }
