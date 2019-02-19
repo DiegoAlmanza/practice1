208764519a5fc3473834893ad967ac32ab9c888d
DiegoAlmanza <46726320+DiegoAlmanza@users.noreply.github.com>
2019-02-18 23:16:36 -0700

    Add files via upload

---
+++ Programa 26 octal a bnario/programa26_octalabinario.java
@@ -0,0 +1,20 @@
+//Nombre: Diego Almanza Beyless
+//Dia: 19/02/2019
+//Hora: 23:12
+ import java.util.Scanner;//Importamos la libreria java.util para usar la clase Scanner para poder leer valores de entrada
+
+class programa26{
+    public static void main(String[] args) {
+	Scanner sc= new Scanner(System.in);//Se crea el Scanner para poder ingresar datos desde el teclado y poder almacenarlos.
+	
+	System.out.print("Ingrese el numero octal: ");//Pedimos que se ingrese el numero octal.
+	String a= sc.nextLine();//El scanner permite ingresar el numero al usuario y almacenarlo en a.
+	
+	
+	int a1=Integer.parseInt(a, 8);//Se convierte el numero octal a decimal.
+	String b=Integer.toBinaryString(a1);//a1 Se convierte en numero binario
+	
+	System.out.print("Numero Decimal: "+b);//Mostramos el resultado.
+
+}
+}
