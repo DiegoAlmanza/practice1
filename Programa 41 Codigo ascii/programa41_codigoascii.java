6be944dda21cc9b0b47126bfe9dfc154c904b8b9
DiegoAlmanza <46726320+DiegoAlmanza@users.noreply.github.com>
2019-02-23 15:17:50 -0700

    Add files via upload

---
+++ Programa 41 Codigo ascii/programa41_codigoascii.java
@@ -0,0 +1,15 @@
+//Nombre: Diego Almanza Beyless
+//Dia: 23/02/2019
+//Hora: 14:48
+
+import java.util.Scanner;////Importamos la libreria java.util para usar la clase Scanner para poder leer valores de entrada
+
+class programa41{
+public static void main(String[] String){
+
+	int a='z';
+
+	System.out.print("El codigo ascii es: "+a);//Mostramos el resultado
+
+  }
+}
