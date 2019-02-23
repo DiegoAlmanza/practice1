0386d71de480fc33e85e2f15340c3425fb927f25
DiegoAlmanza <46726320+DiegoAlmanza@users.noreply.github.com>
2019-02-23 03:03:21 -0600

    Update programa38.java

--- Programa 38 Contar caracteres/programa38.java
+++ Programa 38 Contar caracteres/programa38.java
@@ -11,28 +11,29 @@ public static void main(String[] args){
 	System.out.print("Ingrese una cadena: ");
 	String S=sc.nextLine();
 
-	char[] ch= S.toCharArray();
+	char[] ch= S.toCharArray();//Guardamos la cadena en un arreglo.
+	//declaramos las variables.
 	int letras= 0;
 	int espacios= 0;
 	int numeros=0;
 	int otros=0;
 
-	for(int i=0; i<S.length();i++){
-		if(Character.isLetter(ch[i])){
+	for(int i=0; i<S.length();i++){//length cuenta los caracteres de la cadena
+		if(Character.isLetter(ch[i])){//Si el caracter es una letra aumentamos el valor letras.
 			letras++;
 		}
-		else if(Character.isSpaceChar(ch[i])){
+		else if(Character.isSpaceChar(ch[i])){//si el caracter es un espacio aumentamos el valor de espacios.
 			espacios++;
 		}
-		else if(Character.isDigit(ch[i])){
+		else if(Character.isDigit(ch[i])){//si el caracter es un numero aumentamos el valor de numeros.
 			numeros++;
 		}
 		else{
-			otros++;
+			otros++;//si no es ninguno aumentamos el valor de otros.
 		}
 			
 	}
-
+	//Mostramos los resultados.
 	System.out.print("\n"+S);
 	System.out.print("\nTiene "+letras+" letras");
 	System.out.print("\nTiene "+espacios+" espacios ");
