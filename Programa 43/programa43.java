7ac970b64f31d19c4379007c20c393ed5e8066e9
DiegoAlmanza <46726320+DiegoAlmanza@users.noreply.github.com>
2019-02-23 21:48:58 -0700

    Add files via upload

---
+++ Programa 43/programa43.java
@@ -0,0 +1,15 @@
+//Nombre: Diego Almanza Beyless
+//Fecha: 23/02/2019
+//Hora: 21:35
+
+class programa43 {
+	public static void main(String[] args) {
+	
+	System.out.println("\nTwinkle, twinkle, little star, "); 
+	System.out.print("\tHow I wonder what you are! ");
+	System.out.print("\n\t\tUp above the world so high, ");
+	System.out.print("\n\t\tLike a diamond in the sky. "); 
+	System.out.print("\nTwinkle, twinkle, little star,");
+	System.out.print(" \n\tHow I wonder what you are!\n\n");
+}
+}
