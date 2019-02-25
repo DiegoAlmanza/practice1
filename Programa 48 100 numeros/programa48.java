1bc709e0800dedb962f0e2ef9f94392d1f917e10
DiegoAlmanza <46726320+DiegoAlmanza@users.noreply.github.com>
2019-02-24 20:07:37 -0700

    Add files via upload

---
+++ Programa 48 100 numeros/programa48.java
@@ -0,0 +1,15 @@
+//Nombre: Diego Almanza Beyless
+//Fecha: 24/02/2019
+//Hora: 19:40
+
+class programa48{
+ public static void main(String[] args) {
+	
+	for(int i=1;i<=100;i++){
+		
+		System.out.print(i+"\n");
+
+     }
+	
+  }
+}
