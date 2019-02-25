f73820ec0f869f72f8324b70f43b95ca25b17478
DiegoAlmanza <46726320+DiegoAlmanza@users.noreply.github.com>
2019-02-24 19:42:45 -0700

    Add files via upload

---
+++ Programa 46 Hora actual/programa46.java
@@ -0,0 +1,10 @@
+//Nombre: Diego Almanza Beyless
+//Fecha: 24/02/2019
+//Hora: 19:38
+
+class programa46{
+ public static void main(String[] args) {
+	
+	System.out.format("\nTiempo actual: %tc%n\n", System.currentTimeMillis()); 
+  }
+}
